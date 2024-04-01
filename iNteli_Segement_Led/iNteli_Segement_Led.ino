fc79b14932cb8f5cac9a991b26c50febacb1f66b
Nyein Chan Ko <nove0943@gmail.com>
2024-04-01 16:54:03 +0630

    add ds18b20

--- iNteli_Segement_Led/iNteli_Segement_Led.ino
+++ iNteli_Segement_Led/iNteli_Segement_Led.ino
@@ -1,5 +1,17 @@
 #include <LedControl.h>
 
+// Include the libraries we need
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is plugged into port 2 on the Arduino
+#define ONE_WIRE_BUS D4
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+// Pass our oneWire reference to Dallas TemperatureDallasTem
+
 // Define the number of MAX7219 devices in your setup
 #define MAX_DEVICES 1
 
@@ -35,38 +47,55 @@ void displayFloat(int value, unsigned int row = 0, unsigned int decimalPlaces = 
   Serial.print("\t");
   Serial.print(value);
   unsigned int v = (unsigned int) (value < 0 ? -value : value);
+
+
   Serial.print("\t");
   Serial.print("v = ");
   Serial.print("\t");
   Serial.println(v);
-  for (unsigned int i = 0; i <=2; i++) {
-    offsetvalue = offsetvalue + 1;
-    Serial.print("OFFsetValue = ");
-    Serial.print("\t");
-    Serial.print(offsetvalue);
-    Serial.print("\t");
-    Serial.print("V % 10 = ");
-    Serial.print("\t");
-    Serial.println(v % 10);
-    //    if (offsetvalue > 5) {
-    //      Serial.print("V % 10 = ");
-    //      Serial.print("\t");
-    //      Serial.println(v % 10);
-    //      Serial.print("offset Value = ");
-    //      Serial.print(offsetvalue -1);
-    //      lc.setDigit(row, offsetvalue - 1, v % 10, i == decimalPlaces);
-    //    } else
+  if (value < 0) {
+    for (unsigned int i = 0; i <= 2; i++) {
+      offsetvalue = offsetvalue + 1;
+      Serial.print("OFFsetValue = ");
+      Serial.print("\t");
+      Serial.print(offsetvalue);
+      Serial.print("\t");
+      Serial.print("V % 10 = ");
+      Serial.print("\t");
+      Serial.println(v % 10);
+      lc.setDigit(row, offsetvalue , v % 10, false);
+      if (v % 10 == 0) {
+        lc.setChar(row, offsetvalue, '-', false);
+      }
+      if (offsetvalue == 7) offsetvalue = 4;
 
-    lc.setDigit(row, offsetvalue - 1 , v % 10, false);
-    if (offsetvalue >7) offsetvalue = 4;
+      v /= 10;
+      Serial.println(v / 10);
 
-    v /= 10;
-    Serial.println(v/10);
-  
+    }
+
+  } else {
+    for (unsigned int i = 0; i <= 2; i++) {
+      offsetvalue = offsetvalue + 1;
+      Serial.print("OFFsetValue = ");
+      Serial.print("\t");
+      Serial.print(offsetvalue);
+      Serial.print("\t");
+      Serial.print("V % 10 = ");
+      Serial.print("\t");
+      Serial.println(v % 10);
+
+      if (offsetvalue == 7) offsetvalue = 4;
+      lc.setDigit(row, offsetvalue, v % 10, false);
+      //  if (offsetvalue == 7) offsetvalue = 4;
+      v /= 10;
+      Serial.println(v / 10);
+
+    }
   }
 }
 
-int temp = -23;
+int temp = -30.5;
 void setup() {
   Serial.begin(9600);
   // Initialize the MAX7219 device
@@ -74,44 +103,37 @@ void setup() {
   lc.setIntensity(0, 8);       // Set brightness level (0 is min, 15 is max)
   lc.clearDisplay(0);          // Clear the display
   Serial.println(sizeof(temp));
+
+  // Start up the library
+  sensors.begin();
 }
 
 char data[4];
 float value;
 void loop() {
-temp = temp - 1;
-Serial.println(temp);
-  Serial.println(sizeof(temp));
-  displayFloat(temp, 0, 1);
-  delay(1000);
-  displayFloat(-34,0,1);
-  
+
+  Serial.print("Requesting temperatures...");
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  Serial.println("DONE");
+  int tempC = sensors.getTempCByIndex(0);
+
+  if (tempC != DEVICE_DISCONNECTED_C)
+  {
+    Serial.print("Temperature for the device 1 (index 0) is: ");
+    Serial.println(tempC);
+  }
+  else
+  {
+    Serial.println("Error: Could not read temperature data");
+  }
+
+  Serial.println(tempC);
+  Serial.println(sizeof(tempC));
+  displayFloat(tempC, 0, 1);
 
 
 
-  ////
-  ////  // Display "HELLO" on the segment LED display
-  ////  lc.setChar(0, 0, 'H', true); // Display 'H' at position 0
-  ////  lc.setChar(0, 1, 'E', true); // Display 'E' at position 1
-  ////  lc.setChar(0, 2, 'L', true); // Display 'L' at position 2
-  ////  lc.setChar(0, 3, 'L', true); // Display 'L' at position 3
-  ////  lc.setChar(0, 4, 'O', true); // Display 'O' at position 4
-  //  lc.setChar(0, 5, 'L', true); // Display 'H' at position 0
-  //  lc.setChar(0, 6, 'E', true); // Display 'E' at position 1
-  //  lc.setChar(0, 7, 'H', true); // Display 'L' at position 2
-  ////  lc.setChar(0, 8, 'H', true); // Display 'L' at position 3
-  ////  lc.setChar(0, 9, 'O', true); // Display 'O' at position 4
-  ////  delay(1000);
-  //
-  //  // Test the display
-  //  for (int i = 0; i < 8; i++) {
-  //    lc.setDigit(0, i, i, false); // Display numbers 0 to 7 sequentially
-  //    delay(500);
-  //  }
-  //   lc.clearDisplay(0);
-  //    for (int i = 8; i > 0; i--) {
-  //    lc.setDigit(0, i-8, i, false); // Display numbers 0 to 7 sequentially
-  //    delay(500);
-  //  }
+
+
 
 }
